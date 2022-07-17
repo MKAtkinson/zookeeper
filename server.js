a197c3d818b274681858410a5cadc32e444137cc
MKAtkinson <maxatkinson@ymail.com>
2022-07-17 16:22:22 -0500

    Front end intergration complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
+app.use(express.static('public'))
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -92,8 +93,23 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals)
   res.json(animal)
   }
-  
 })
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
